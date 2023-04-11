8bedff796c6a09351e1bb10a8d9baa5908e63b92
Geraldinhootieno <98090203+Geraldinhootieno@users.noreply.github.com>
2023-04-11 14:56:33 +0300

    Add files via upload

--- comment.php
+++ comment.php
@@ -1,17 +1,19 @@
 <?php
 $Name = "Name";
+$Email = "Email";
 $Message = "Message";
-$conn = new mysqli('localhost','root','','customer');
+$conn = new mysqli('localhost','root','','Austine');
 if($conn->connect_error)
 {
-	die('Connection failed:'.$conn ->connect_error);
+  die('Connection failed:'.$conn ->connect_error);
 }
 echo "";
 $Name = $_POST['Name'];
+$Email = $_POST['Email'];
 $Message = $_POST['Message'];
-$sql = "insert into comments (Name,Message)values('$Name','$Message')";
+$sql = "insert into comment (Name,Email,Message)values('$Name','$Email','$Message')";
 if ($conn->query($sql) === TRUE) {
-  echo "Comment sent";
+  echo "Comment succesfuly sent and saved";
 } else {
   echo  "Error: " . $sql . "<br>" . $conn->error;
 }
